54315fe2e3142c5d3693bf89a79eaa2fd7b4722b
Mona Lisa <kmazjanis@gmail.com>
2019-06-05 21:58:19 +0300

    Adding missing comments

--- javascript.js
+++ javascript.js
@@ -1,38 +1,38 @@
-
+  /// ADD CLOCK
 var myVar = setInterval(myTimer, 1000);
-
   function myTimer() {
       let d = new Date();
       var t = d.toLocaleTimeString('en-GB');
       document.getElementById("main").innerHTML = t;
     }
 
-  ///
+  /// DISPLAY START TIME IN HTML & SAVE IN LOCAL STORAGE
   function startTime(){
     let time = new Date().toLocaleTimeString('en-GB'); //get only time from date
     document.getElementById("startingTimeOutput").innerHTML = time;
     window.localStorage.setItem("date", time); //Saving start time in localstorage for later use.
   }
 
-  ///SAVING DATA IN LOCAL STORAGE FROM LAST FIRST SAVED TIME
+  /// DISPLAY LAST SAVED STARTING TIME IN HTML
   window.onload = function (){
     let fromlocal = window.localStorage.getItem('date');
     document.getElementById("startingTimeOutput").innerHTML = fromlocal;
   }
 
-
-
+  /// DISPLAY STOP TIME IN HTML
   function stopTime(){
     let date2 = new Date().toLocaleTimeString('en-GB');
     document.getElementById("stopingTimeOutput").innerHTML = date2;
     console.log(date2);
   }
 
+  /// TURN REGULAR TIME IN SECONDS
   function parseTime(s) {
     var c = s.split(':');
     return parseInt(c[0]) * 3600 + parseInt(c[1]) * 60 + parseInt(c[2]);
  }
 
+  /// GET START AND STOP TIME FROM HTML AND CALCULATE DIFFERENCE
   function difInTime(){
     var date1 =  document.getElementById("startingTimeOutput").innerHTML
     var date2 =  document.getElementById("stopingTimeOutput").innerHTML
@@ -41,41 +41,8 @@ var myVar = setInterval(myTimer, 1000);
   }
 
 
-firestore = firebase.firestore(); //ref to firestore
-
-
-  //docuement reference
-
-const output = document.querySelector("#fbOutput");
-const input= document.querySelector("#fbinputText");
-const button = document.querySelector("#fbButtonId");
-const Lbutton = document.querySelector("#LfbButtonId");
-
-//CHANGE STATUS IN DB AND LOAD THIS STATUS FROM FIREBASE TO DOM
-
-// button.addEventListener("click", function (){
-//   const textToSave = input.value;
-//   console.log("sss: " + textToSave);
-//   docRef.set({status: textToSave}).then(function() {
-//     console.log("status saved");
-//   });
-// });
-
-// Lbutton.addEventListener("click", function(){
-//   docRef.get().then(function (doc){
-//     if (doc && doc.exists){
-//       const myData = doc.data();
-//       output.innerHTML = "Something guys " + myData.status;
-//     }
-//   });
-// });
-
-
 ///WRITE DATA TO FIREBASE
-
 function fbButton(){
-  // var firebaseRef = database().ref();
-  // firebaseRef.child("Text").set("somevalue")
 
   var type = document.getElementById("fbinputText").value;
   var starttime = document.getElementById("startingTimeOutput").innerHTML;
@@ -83,7 +50,7 @@ function fbButton(){
   var timedif = document.getElementById("TimeDiff").innerHTML;
   let Fulldate = new Date();
 
-  firestore.collection("Work").doc().set({
+  db.collection("Work").doc().set({
       Type: type,
       StartTime: starttime,
       EndTime: endtime,
@@ -99,7 +66,7 @@ function fbButton(){
 }
   
 
-///LOGIN IN
+ /// LOGIN IN
 
 auth.onAuthStateChanged(user => {
    
@@ -118,40 +85,30 @@ auth.onAuthStateChanged(user => {
 });
 
 
-//SIGNUP
+ /// SIGNUP
 const signupForm = document.querySelector('#signupForm');
 
 signupForm.addEventListener('submit', (e) =>{
-  e.preventDefault();
+    e.preventDefault();
 
-  //get user info
-  const email = signupForm['signup-email'].value;
-  const pass = signupForm['signup-password'].value;
+  /// get user info
+    const email = signupForm['signup-email'].value;
+    const pass = signupForm['signup-password'].value;
 
-
-
-  //sign up the user
-  auth.createUserWithEmailAndPassword(email, pass).then(cred =>{ //takes time to complete so we need to wait
-      console.log(cred.user);
-      
+  /// sign up the user
+    auth.createUserWithEmailAndPassword(email, pass).then(cred =>{ //takes time to complete so we need to wait
+        console.log(cred.user);
   }); 
 });
 
-//LOGOUT
+ /// LOGOUT
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) =>{
   e.preventDefault();
   auth.signOut();
-  // .then(() =>{
-  //     console.log("user signed out");
-  //     document.getElementById("signupDiv").style.display = "visible";
-  //     document.getElementById("logoutDiv").style.display = "none";
-  //     document.getElementById("loginDiv").style.display = "visible";
-      
-  // });
 });
 
-//LOGIN
+ /// LOGIN
 const login = document.querySelector('#loginForm');
 login.addEventListener('submit', (e) =>{
   e.preventDefault();
@@ -159,105 +116,34 @@ login.addEventListener('submit', (e) =>{
   const email = login['login-email'].value;
   const pass = login['login-password'].value;
 
-
   auth.signInWithEmailAndPassword(email, pass).then(cred =>{
       console.log(cred.user);
-  
   });
 });
 
-///CREATE A COOKIE FOR STORING START TIME
-
-// function createCookie(key) {
-//   var cookie = key;  //escape removes all spaces, semicolas, commas becosuse strings cant caontain them.
-//   document.cookie = cookie;
-//   console.log(cookie);
-//   console.log("Creating new cookie with key: " + key);
-// }
 
 
+/// GET REAL TIME DATA FROM DB
 
-
-//create element and render to dom ?
-
-// function render(){
-//   let li = document.createElement('div');   //for each document we create a div tag
-//   let name = document.createElement('span');
-
-//   li.setAttribute('data-id', doc.id); //we set attribute for each id
-  
-//   name.textContent = doc.data().name;  
-
-//   li.appendChild(name);
-
-//   statsOut.appendChild(li);
-  
-// }
-
-
-///GET DATA FROM DB
-// function getStats(){
-
-  
-//   firestore.collection("Work").get().then((snapshot) => {  //snapshot of work collection
-//     snapshot.docs.forEach(doc =>{ //get all the doc from that collection then cycle through them with forEach
-//       // render(doc);  //for each document we call function "render" and passsing in doc as parameter
-//       console.log(doc.data().Type);
-//       console.log(doc.data().TimeDiff);
-  
-//       var test = doc.data().Type;
-      // var plainSeconds = doc.data().TimeDiff;
-      // var time = formatTime(plainSeconds);
-//       document.getElementById("statsOutput").style.display = "none";
-//       document.getElementById("statsOutput").innerHTML += "</br>" + test + " " + time;
-  
-//     });
-//   });
-// };
 const outputStats = document.querySelector("#statsOutput");
 
-
-db.collection("Work").onSnapshot(function(querySnapshot) {
-        querySnapshot.docChanges().forEach(function(change, i) {
-         
-
-            if(change.type === "added"){       
-              if(change.doc.data().Type === 'running'){
-                var plainSeconds = change.doc.data().TimeDiff;
-                var time = formatTime(plainSeconds);
-                outputStats.innerHTML +=  "<span class='troll'>Type: " + change.doc.data().Type + " - " + time + "</span><br>";
-              }
-            }      
-        });        
-  });
-
-  var Stats = document.getElementsByClassName('troll');
-
-    
-  console.log(Stats);
-
-
-
-
-
-
-
-
-// const outputStats2 = document.querySelector("#statsOutput2");
-
-// db.collection("Work").where("Type", "==", "running")
-//     .onSnapshot(function(querySnapshot) {
-//         querySnapshot.forEach(function(doc) {
-//             let first = doc.data().Type;
-//             let test = doc.data().TimeDiff;
-           
-//         });
+db.collection("Work").where("TimeDiff", ">=", "1")
+    .onSnapshot(function(querySnapshot) {
+        var cities = [];
+        querySnapshot.forEach(function(doc) {
+            cities.push(doc.data().TimeDiff);
+        });
         
-//     });
+        const citiNumber = cities.map(Number);
+        const result = citiNumber.reduce((a, b) => a + b); 
+        
+        document.getElementById("statsOutput").innerHTML = `Live result: ${formatTime(result)}`;
+});
 
 
 
-//FROM SECONDS BACK TO TIME FUNCTION
+
+//FROM SECONDS BACK TO TIME
 function formatTime(seconds) {
   const h = Math.floor(seconds / 3600);
   const m = Math.floor((seconds % 3600) / 60);
@@ -271,12 +157,4 @@ function formatTime(seconds) {
 
 
 
-
-// firestore.collection("Work").get().then(function(querySnapshot) {
-//   querySnapshot.forEach(function(doc) {
-//       // doc.data() is never undefined for query doc snapshots
-//       console.log(doc.data());
-      
-//   });
-// });
 
